019d0c2ba74d9b66c0d96861aa48ec78fe59185a
Rolands <rolands.laucis@gmail.com>
2025-02-12 18:28:46 +0100

    less indicators & non na files

--- indicators.py
+++ indicators.py
@@ -47,16 +47,20 @@ for j, time_scale in enumerate(scales):
 
         df = pd.read_csv(csv)
         try:
-            df['EMA'] = ta.ema(df['Close'], length=14)
+            df['EMA'] = ta.ema(df['Close'], length=21)
 
-            stoch_rsi = ta.stochrsi(df['Close'], length=14)
-            df['Stoch_RSI'] = stoch_rsi['STOCHRSIk_14_14_3_3']
-            df['Stoch_RSI_D'] = stoch_rsi['STOCHRSId_14_14_3_3']
+            stoch_rsi = ta.stochrsi(df['Close'])
+            # print(stoch_rsi.head())
+            # exit(0)
+            df['Stoch_RSI'] = stoch_rsi.iloc[:, 0]
+            df['Stoch_RSI_D'] = stoch_rsi.iloc[:, 1]
 
-            macd = ta.macd(df['Close'])
-            df['MACD'] = macd['MACD_12_26_9']
-            df['MACD_Signal'] = macd['MACDs_12_26_9']
-            # df['MACD_Hist'] = macd['MACDh_12_26_9']
+            macd = ta.macd(df['Close'])#.dropna()
+            # print(macd.head())
+            # exit(0)
+            df['MACD'] = macd.iloc[:, 0]
+            # df['MACD_Hist'] = macd.iloc[:, 1]
+            df['MACD_Signal'] = macd.iloc[:, 2]
 
             df['SAR'] = ta.psar(df['High'], df['Low'], df['Close'])['PSARl_0.02_0.2']
             df['SuperTrend'] = ta.supertrend(df['High'], df['Low'], df['Close'])['SUPERT_7_3.0']
@@ -66,31 +70,37 @@ for j, time_scale in enumerate(scales):
             df['BOLL_Low'] = bb.iloc[:, 0]
             # df['BOLL_Mid'] = bb.iloc[:, 1]
             df['BOLL_High'] = bb.iloc[:, 2]
-            # df['WR_WR2'] = ta.wr(df['High'], df['Low'], df['Close'], length=14)  # Williams %R with default length
+            # df['WR_WR2'] = ta.wr(df['High'], df['Low'], df['Close'])  # Williams %R with default length
 
             ## Energy Indicators
-            df['PSL'] = ta.psl(df['Close'], length=14)  # Psychological Line (default length = 12)
+            df['PSL'] = ta.psl(df['Close'])  # Psychological Line (default length = 12)
             df['MASS'] = ta.massi(df['High'], df['Low'])  # Mass Index (default settings)
+            # print(ta.massi(df['High'], df['Low']).dropna().head())
+            # exit(0)
 
             ## Volatility Indicators
-            df['NATR'] = ta.natr(df['High'], df['Low'], df['Close'], length=14)  # Normalized ATR
+            df['NATR'] = ta.natr(df['High'], df['Low'], df['Close'])  # Normalized ATR
             # df['TRANGE'] = ta.trange(df['High'], df['Low'], df['Close'])  # True Range
 
             ## Momentum Indicators
-            df['ROC'] = ta.roc(df['Close'], length=14)  # Rate of Change (default length = 12)
-            df['WILLR'] = ta.willr(df['High'], df['Low'], df['Close'], length=14)  # Williams %R
+            df['ROC'] = ta.roc(df['Close'])  # Rate of Change
+            df['WILLR'] = ta.willr(df['High'], df['Low'], df['Close'])  # Williams %R
 
-            dmi = ta.adx(df['High'], df['Low'], df['Close'], length=14)
-            df['DMI_ADX'] = dmi['ADX_14']
-            df['DMI_DI_P'] = dmi['DMP_14']
-            df['DMI_DI_N'] = dmi['DMN_14']
+            dmi = ta.adx(df['High'], df['Low'], df['Close']).dropna()
+            # print(dmi.head())
+            # exit(0)
+            df['DMI_ADX'] = dmi.iloc[:, 0]
+            df['DMI_DI_P'] = dmi.iloc[:, 1]
+            df['DMI_DI_N'] = dmi.iloc[:, 2]
 
             ## Hilbert Transform - Dominant Cycle Period and Phase
             # df['HT_DCPERIOD'] = ta.ht_dcperiod(df['Close'])  # Dominant Cycle Period
             # df['HT_DCPHASE'] = ta.ht_dcphase(df['Close'])    # Dominant Cycle Phase
 
-            brar = ta.aroon(df['High'], df['Low'], length=14)
-            df['AR'] = brar['AROOND_14']  # Approximate AR value using Aroon Down or adapt as needed
+            # brar = ta.aroon(df['High'], df['Low']).dropna()
+            # print(brar.head())
+            # exit(0)
+            # df['AR'] = brar.iloc[:, 0]  # Approximate AR value using Aroon Down or adapt as needed
 
             if time_scale == 'days':
                 # Convert the date column to a cyclical year 1-364 day representation
@@ -101,17 +111,18 @@ for j, time_scale in enumerate(scales):
                 df['Date'] = (df['Date'] - timestamp_2000).dt.days
                 df.drop(columns=['Day'], inplace=True)
 
-            df = df[['Date', 'Date_sin', 'Date_cos', 'Open', 'High', 'Low', 'Close', 'EMA', 'Stoch_RSI', 'Stoch_RSI_D',  'SAR', 'SuperTrend','BOLL_High','BOLL_Low', 'MACD', 'MACD_Signal','WILLR','PSL','MASS','NATR','ROC','DMI_ADX','DMI_DI_P','DMI_DI_N','AR']] #
-            df = df[14:,:]
-            # df.dropna(inplace=True)
+            df = df[['Date', 'Date_sin', 'Date_cos', 'Open', 'High', 'Low', 'Close', 'EMA', 'Stoch_RSI', 'Stoch_RSI_D',  'SAR', 'SuperTrend','BOLL_High','BOLL_Low', 'MACD', 'MACD_Signal','WILLR','PSL','MASS','NATR','ROC','DMI_ADX','DMI_DI_P','DMI_DI_N']] # ,'AR'
+            # df = df.iloc[l:,:]#.dropna()
+            df.dropna(inplace=True)
             # print(df.head())
             # exit(0)
+            if df.empty: continue
             df.to_csv(f'./data/indicators/{time_scale}/{file_id}', index=False)
             processed_files[time_scale].add(file_id)
         except Exception as e:
             # print(traceback.format_exc())
             # exit(1)
-            pass
+            continue
 
         if i % 20 == 0:
             print(f"Processed {round(((i+1)/count) * 100, 2) }%", end='\r')
